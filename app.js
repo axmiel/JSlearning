a176a8efd878caa69eea1d03d9aba6bcfd287755
Arthur Smielewski <101211171+axmiel@users.noreply.github.com>
2023-12-19 20:58:44 +0000

    app changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*/#### lesson 1 - console logs, alerts, etc####
+/*//#### lesson 1 - console logs, alerts, etc####
 console.log("lesson 1");
 window.console.log("window is an object with many properties");
 window.alert("alerts can be displayed using the window object");
@@ -13,16 +13,16 @@ console.warn("omitted one line of code"); //prints out a warning in the console
 console.info("this is my first JS project"); //prints out an information in the console
 
 {
-    console.warn("this is a block of code");
+    console.warn("block starts here: this is a block of code");
     console.log("blocks are used to group related code together");
-    console.log("still the same block")
+    console.log("still the same block -")
 }
 */
 
 //#### lesson 2 variables, consts & prompts####
-alert("prompt displays an alert that takes in user input");
-console.log("prompt input can be stored in a variable");
-let name = prompt("input your name");
+alert("prompt() displays an alert that takes in user input");
+console.log("prompt() input can be stored in a variable");
+let name = prompt("this is a prompt(), the input will be assigned to a variable 'name'");
 console.warn("user: " + name + " entered the site");
 
 //let is used to declare mutable variables
