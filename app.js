bb7ba3f40865b978879932b6cbdb9460170a6da1
Arthur Smielewski <101211171+axmiel@users.noreply.github.com>
2024-06-25 15:50:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let numberOfButtons = randomNumber(100,200);
+    let numberOfButtons = randomNumber(10, 25);
 
     function randomNumber(min, max) {
         const minCeiled = Math.ceil(min);
@@ -7,36 +7,34 @@ document.addEventListener('DOMContentLoaded', function() {
         return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled)
     }
     
+    const perf = performance.now()
+
     const contentSection = document.querySelector(".content");
 
     for (i=0; i<numberOfButtons; i++) {
-        const element = document.createElement("button")
-        const node = document.createTextNode("Button " + i);
-        element.append(node);
+        let element = document.createElement("button");
+        element.textContent = "Button " + i;
         element.setAttribute("class", "button");
+        if (i == 0) {
+            element.style.border = "2px solid grey";
+            element.style.backgroundColor = "black";
+            element.style.color = "white";
+        } else if (i % 3 == 0) {
+            element.style.border = "2px solid hsl(165, 100%, 20%)";
+            element.style.backgroundColor = "hsl(165, 100%, 70%)";
+            element.style.color = "hsl(165, 100%, 12%)";
+        } else if (i % 2 == 0) {
+            element.style.border = "2px solid hsl(0, 100%, 70%)";
+            element.style.backgroundColor = "hsl(0, 100%, 28%)";
+            element.style.color = "gainsboro";
+        } else {
+            element.style.border = "2px solid hsl(197, 100%, 30%)";
+            element.style.backgroundColor = "hsl(197, 100%, 70%)";
+            element.style.color = "hsl(197, 100%, 12%)";
+        }
         contentSection.append(element);
     }
 
-    const buttons = document.querySelectorAll("button");
-
-    for (i=0; i<buttons.length; i++) {
-        if (i == 0) {
-            buttons[i].style.border = "2px solid grey";
-            buttons[i].style.backgroundColor = "black";
-            buttons[i].style.color = "white";
-        } else if (i % 3 == 0) {
-            buttons[i].style.border = "2px solid hsl(165, 100%, 20%)";
-            buttons[i].style.backgroundColor = "hsl(165, 100%, 70%)";
-            buttons[i].style.color = "hsl(165, 100%, 12%)";
-        } else if (i % 2 == 0) {
-            buttons[i].style.border = "2px solid hsl(0, 100%, 70%)";
-            buttons[i].style.backgroundColor = "hsl(0, 100%, 28%)";
-            buttons[i].style.color = "gainsboro";
-        } else {
-            buttons[i].style.border = "2px solid hsl(197, 100%, 30%)";
-            buttons[i].style.backgroundColor = "hsl(197, 100%, 70%)";
-            buttons[i].style.color = "hsl(197, 100%, 12%)";
-        }
-    }
+    console.log('Time to execute: ' + (performance.now() - perf));
 });
 
